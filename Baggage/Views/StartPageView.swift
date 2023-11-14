67d93d265cde4c0118996e60f1573ec30a4b3e6c
Youngmin Koo <youngminz.kr@gmail.com>
2023-11-15 01:52:30 +0900

    폰트 색깔 적용

--- Baggage/Views/StartPageView.swift
+++ Baggage/Views/StartPageView.swift
@@ -3,11 +3,25 @@ import SwiftUI
 struct StartPageView: View {
     var body: some View {
         VStack {
+            Spacer()
+            
             Text("우리만의 수하물 스캐너")
                 .font(.custom("PoorStory-Regular", size: 18))
+                .foregroundColor(Color(hex: "7C7C7C"))
             
             Text("수캐너")
                 .font(.custom("PoorStory-Regular", size: 24))
+                .foregroundColor(Color(hex: "7C7C7C"))
+            
+            Spacer()
+
+            // 사진
+
+            Spacer()
+
+            // 시작하기 버튼
+
+            Spacer()
         }
     }
 }
