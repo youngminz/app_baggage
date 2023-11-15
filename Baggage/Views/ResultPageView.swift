99f16998960a3877ad2b4028697a6e149ee0ba15
Youngmin Koo <youngminz.kr@gmail.com>
2023-11-16 01:20:36 +0900

    결과 화면에 사진 추가

--- Baggage/Views/ResultPageView.swift
+++ Baggage/Views/ResultPageView.swift
@@ -14,7 +14,7 @@ struct ResultPageView: View {
                 }) {
                     Image("NavigationBarBackButton")
                 }
-                .padding()
+                .padding(.leading, 10)
 
                 Spacer()
             }
@@ -23,6 +23,22 @@ struct ResultPageView: View {
                 Text("촬영 시 한 화면에 하나의 물품만 나오게 촬영해주세요")
                     .font(.custom("Jua-Regular", size: 16))
                     .foregroundColor(Color(hex: "4F4F4F"))
+                
+                ZStack {
+                    Color(hex: "F0F8FE")
+                    
+                    VStack {
+                        Image(uiImage: takenImage)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        Text("스프레이류")
+                            .font(.custom("Jua-Regular", size: 16))
+                            .padding(.vertical, 15)
+                    }
+                }
+                .border(Color(hex: "97CBEB"), width: 2)
+                .padding(.all, 5)
             }
         }
         .navigationBarBackButtonHidden(true)
