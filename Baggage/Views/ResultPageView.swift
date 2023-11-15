9a1d6fa3939e3f08d3438ec65d970ab43e7d6eb6
Youngmin Koo <youngminz.kr@gmail.com>
2023-11-16 01:06:36 +0900

    결과 화면 시작

--- Baggage/Views/ResultPageView.swift
+++ Baggage/Views/ResultPageView.swift
@@ -4,9 +4,32 @@ struct ResultPageView: View {
     @State var takenImage: UIImage
     @StateObject var mainViewModel: MainViewModel
     
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        NavigationView {
-            
+        VStack {
+            HStack {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image("NavigationBarBackButton")
+                }
+                .padding()
+
+                Spacer()
+            }
+
+            ScrollView() {
+                Text("촬영 시 한 화면에 하나의 물품만 나오게 촬영해주세요")
+                    .font(.custom("Jua-Regular", size: 16))
+                    .foregroundColor(Color(hex: "4F4F4F"))
+            }
         }
+        .navigationBarBackButtonHidden(true)
     }
+        
+}
+
+#Preview {
+    ResultPageView(takenImage: UIImage(), mainViewModel: MainViewModel())
 }
