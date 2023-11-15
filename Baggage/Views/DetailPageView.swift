9fa7b5f86f23a4ede21b30ed8026aff2e14b2c59
Youngmin Koo <youngminz.kr@gmail.com>
2023-11-15 22:18:24 +0900

    NavigationView 적용

---
+++ Baggage/Views/DetailPageView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailPageView.swift
+//  Baggage
+//
+//  Created by 구영민 on 11/15/23.
+//
+
+import Foundation
--- Baggage/Views/StartPageView.swift
+++ Baggage/Views/StartPageView.swift
@@ -5,36 +5,38 @@ struct StartPageView: View {
     @State private var pickedImage: UIImage?
     
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Text("우리만의 수하물 스캐너")
-                .font(.custom("PoorStory-Regular", size: 18))
-                .foregroundColor(Color(hex: "7C7C7C"))
-            
-            Text("수캐너")
-                .font(.custom("PoorStory-Regular", size: 24))
-                .foregroundColor(Color(hex: "7C7C7C"))
-            
-            Spacer()
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                Text("우리만의 수하물 스캐너")
+                    .font(.custom("PoorStory-Regular", size: 18))
+                    .foregroundColor(Color(hex: "7C7C7C"))
+                
+                Text("수캐너")
+                    .font(.custom("PoorStory-Regular", size: 24))
+                    .foregroundColor(Color(hex: "7C7C7C"))
+                
+                Spacer()
 
-            Image("StartPageImage")
+                Image("StartPageImage")
 
-            Spacer()
+                Spacer()
 
-            Button(action: {
-                self.showImagePicker = true
-            }) {
-                Image("StartPageButton")
+                Button(action: {
+                    self.showImagePicker = true
+                }) {
+                    Image("StartPageButton")
+                }
+
+                Spacer()
             }
-
-            Spacer()
+            .fullScreenCover(isPresented: $showImagePicker, content: {
+                SUImagePicker { image in
+                    self.pickedImage = image
+                }
+            })
         }
-        .fullScreenCover(isPresented: $showImagePicker, content: {
-            SUImagePicker { image in
-                self.pickedImage = image
-            }
-        })
     }
 }
 
